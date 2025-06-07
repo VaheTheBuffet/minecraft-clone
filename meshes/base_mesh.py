2bbff1cf15ca51d6a2cddf4388e9abc1ce61cac8
ghips <vpetrosyan1342@gmail.com>
2025-06-07 16:28:38 -0700

    initial commit

---
+++ meshes/base_mesh.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class BaseMesh:
+    def __init__(self):
+        self.ctx = None # OpenGl Context
+        self.program = None # shader program
+        self.vbo_format = None # vertex buffer data type format: "3f 3f"
+        #attribute names according to the format: ("in_position", "in_color")
+        self.attrs: [str, ...] = None 
+
+        self.vao = None # vertex array object
+
+    def get_vertex_data(self) -> np.ndarray:
+        return np.array([-1])
+
+    def get_vao(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        vao = self.ctx.vertex_array(
+            self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors = True
+        )
+        return vao
+
+    def render(self):
+        self.vao.render()
